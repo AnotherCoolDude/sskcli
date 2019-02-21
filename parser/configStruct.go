473190b7acebc049d88ae7e5e121bc9bb7f7bce2
Empfang <empfang@Cecile-Casenobe.local>
2019-02-21 12:48:26 +0100

    refactor

--- parser/configStruct.go
+++ parser/configStruct.go
@@ -1,12 +1,19 @@
 package parser
 
+// Config holds the title and the tools of config.yaml
 type Config struct {
 	Title string `yaml:"toolListTitle"`
-	Tools struct {
-		Name    string `yaml:"name"`
-		Actions struct {
-			Name        string `yaml:"name"`
-			Description string `yaml:"desc"`
-		}
-	}
+	Tools []Tool `yaml:"tools"`
+}
+
+// Tool holds the title and the actions of a config tool
+type Tool struct {
+	Name    string   `yaml:"name"`
+	Actions []Action `yaml:"actions"`
+}
+
+// Action holds the name and the description of a tool action
+type Action struct {
+	Name        string `yaml:"name"`
+	Description string `yaml:"desc"`
 }
